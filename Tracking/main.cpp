910a9865f84d2b4593bc37edee8ba06133621b27
yuukifujita <yuukifujita@yuukifujita-2.local>
2015-01-20 01:55:03 +0900

    Initial Commit

---
+++ Tracking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tracking
+//
+//  Created by yuukifujita on 1/20/15.
+//  Copyright (c) 2015 yuukifujita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
